9bf5a368ae91ed08076b84d22c6f4f8c00d46d2e
LeonardodeSouzaFonseca <leonardosouzafonseca902@gmail.com>
2024-08-05 10:18:58 -0300

    Atualizar o Exercicio_01.py

--- exercicio de if_else/Exercicio_01.py
+++ exercicio de if_else/Exercicio_01.py
@@ -4,7 +4,7 @@
 num_1 = int(input("Digite um numero: "))
 num_2 = int(input('Digite outros numero: '))
 if num_1 > num_2:
-    print(f"Numero {num_1} Maior que     {num_2}")
+    print(f"Numero {num_1} Maior que   {num_2}")
 elif num_1 == num_2:
     print("são iguias.")
 else:
